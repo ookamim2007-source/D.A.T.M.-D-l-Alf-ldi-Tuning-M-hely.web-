a77136d524d6dcfa4d193a1321dcad37b0995f40
mor <tmor553@gmail.com>
2026-02-13 09:25:49 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+function myFunction2() {
+  var x = document.getElementById("password2");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
